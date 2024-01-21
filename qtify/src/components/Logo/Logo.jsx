c2a39d5ff74f1e26bf491634d4b32cfe2112826c
santhoshwesley <santhoshwesleyyer@gmail.com>
2024-01-21 15:43:39 +0530

    Logo bug fix outside src

--- qtify/src/components/Logo/Logo.jsx
+++ qtify/src/components/Logo/Logo.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import styles from "./Logo.module.css";
 import LogoImage from "../../assets/qtify_logo.png";
+
 function Logo() {
   return <img src={LogoImage} alt="logo" width={67} />;
 }
+
 export default Logo;
