0b80dc0b24f2fe4295d21ad03db6a5720065fd24
santhoshwesley <santhoshwesleyyer@gmail.com>
2024-01-21 15:36:44 +0530

    Logo bug fix

--- qtify/src/components/Logo/Logo.jsx
+++ qtify/src/components/Logo/Logo.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from "./Logo.module.css";
-import LogoImage from "/CRIO/Qtify/qtify/src/assets/qtify_logo.png";
+import LogoImage from "../../assets/qtify_logo.png";
 function Logo() {
   return <img src={LogoImage} alt="logo" width={67} />;
 }
