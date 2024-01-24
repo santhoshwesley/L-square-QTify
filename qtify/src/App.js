bbcf2c21fb4ae57c17523d287e12c855423f555b
santhoshwesley <santhoshwesleyyer@gmail.com>
2024-01-24 18:06:58 +0530

    Section with new album

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -1,13 +1,58 @@
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
 import Section from "./components/Section/Section";
+import axios from "axios";
+
+async function fetchData(apiEndpoint) {
+  try {
+    const response = await axios.get(apiEndpoint);
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    return [];
+  }
+}
+
 function App() {
+  const topAlbumsEndpoint = "https://qtify-backend-labs.crio.do/albums/top";
+  const newAlbumsEndpoint = "https://qtify-backend-labs.crio.do/albums/new";
+
+  const [topAlbums, setTopAlbums] = useState([]);
+  const [newAlbums, setNewAlbums] = useState([]);
+
+  useEffect(() => {
+    const fetchTopAlbums = async () => {
+      const data = await fetchData(topAlbumsEndpoint);
+      setTopAlbums(data);
+    };
+
+    const fetchNewAlbums = async () => {
+      const data = await fetchData(newAlbumsEndpoint);
+      setNewAlbums(data);
+    };
+
+    fetchTopAlbums();
+    fetchNewAlbums();
+  }, []);
+
   return (
     <>
       <Navbar />
       <Hero />
-      <Section title={`Top Albums`} />
+      <Section
+        title={`Top Albums`}
+        albums={topAlbums}
+        buttonName="Collapse"
+        showDivider={true}
+      />
+      <Section
+        title={`New Albums`}
+        albums={newAlbums}
+        buttonName="Show All"
+        showDivider={false}
+      />
     </>
   );
 }
--- qtify/src/components/Section/Section.jsx
+++ qtify/src/components/Section/Section.jsx
@@ -1,43 +1,19 @@
-import React, { useState, useEffect } from "react";
 import styles from "../Section/Section.module.css";
 import Card from "../Card/Card";
-import axios from "axios";
 
-async function fetchData() {
-  try {
-    const response = await axios.get(
-      "https://qtify-backend-labs.crio.do/albums/top"
-    );
-    return response.data;
-  } catch (error) {
-    console.error("Error fetching data:", error);
-    return null;
-  }
-}
-
-function Section({ title }) {
-  const [albums, setAlbums] = useState([]);
-
-  useEffect(() => {
-    const fetchAlbums = async () => {
-      const data = await fetchData();
-      setAlbums(data);
-    };
-
-    fetchAlbums();
-  }, []);
-
+function Section({ title, albums, buttonName, showDivider }) {
   return (
     <div className={styles.section}>
       <div className={styles.header}>
         <h2 className={styles.sectionTitle}>{title}</h2>
-        <button className={styles.sectionCollapseButtton}> Collpase</button>
+        <button className={styles.sectionCollapseButtton}> {buttonName}</button>
       </div>
       <div className={styles.albumContainer}>
         {albums.map((album) => (
           <Card key={album.id} data={album} type="album" />
         ))}
       </div>
+      {showDivider && <div className={styles.divider}></div>}
     </div>
   );
 }
